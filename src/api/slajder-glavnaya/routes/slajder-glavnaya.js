fa06cf13b8a439af09916206a24f1c31768a2f86
DimaDevelopment <dima.development@wandry.com.ua>
2024-04-15 16:00:04 +0300

    added home slider resource

---
+++ src/api/slajder-glavnaya/routes/slajder-glavnaya.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * slajder-glavnaya router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::slajder-glavnaya.slajder-glavnaya');
---
+++ src/api/slajder-glavnaya/services/slajder-glavnaya.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * slajder-glavnaya service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::slajder-glavnaya.slajder-glavnaya');
