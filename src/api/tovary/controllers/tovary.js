49fb9aa486c4a86086a5614bc3fb236883c3083d
dev_wandry <dima.development@wandry.com.ua>
2026-06-29 11:52:50 +0300

    feat: backward products controller

--- src/api/tovary/controllers/tovary.js
+++ src/api/tovary/controllers/tovary.js
@@ -1,9 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * tovary controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::tovary.tovary');
+module.exports = createCoreController("api::tovary.tovary", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const { populate, locale = strapi.config.get("api.defaultLocale") } =
+      ctx.query;
+
+    console.log("ctx.params:", ctx.params);
+
+    const queryId = isNaN(Number(id)) ? { documentId: id } : { id };
+    console.log("queryId:", queryId);
+    const entity = await strapi.db.query("api::tovary.tovary").findOne({
+      where: {
+        $and: [
+          queryId,
+          {
+            locale,
+          },
+        ],
+      },
+      populate,
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
