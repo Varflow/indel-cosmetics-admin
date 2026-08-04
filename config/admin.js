62121ce2206c633f35514d91ad14e1eded5a5fdc
kapishdima <kapishdima@gmail.com>
2026-08-04 12:11:09 +0300

    fix: increase cookies ttl

--- config/admin.js
+++ config/admin.js
@@ -6,6 +6,11 @@ module.exports = ({ env }) => ({
       // из-за чего браузер не отдаёт refresh-куку и сессия рвётся каждые 30 минут.
       path: env('ADMIN_COOKIE_PATH', '/dashboard/admin'),
     },
+    sessions: {
+      accessTokenLifespan: 60 * 60, // 1 час вместо 30 мин
+      idleSessionLifespan: 8 * 60 * 60, // 8 часов простоя вместо 2
+      maxSessionLifespan: 7 * 24 * 60 * 60, // неделя вместо суток
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
